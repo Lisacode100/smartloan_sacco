fbcae5cba89c33fbaa39e34b012d9a2fe3aad145
Lisacode100 <eageno075@gmail.com>
2025-06-25 22:04:42 +0300

    added widgets

--- lib/main.dart
+++ lib/main.dart
@@ -14,3 +14,29 @@ class SaccoDashboardApp extends StatelessWidget {
     );
   }
 }
+class ErrorApp extends StatelessWidget {
+  final String message;
+  const ErrorApp({super.key, required this.message});
+
+    @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Icon(Icons.error_outline, size: 50, color: Colors.red),
+              const SizedBox(height: 20),
+              Text(
+                message,
+                style: const TextStyle(fontSize: 18),
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
