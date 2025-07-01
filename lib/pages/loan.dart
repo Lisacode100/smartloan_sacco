eed8f323487e521b11bdfd13b03519ffb26ac30c
Lisacode100 <eageno075@gmail.com>
2025-07-01 05:48:58 +0300

    loan details

---
+++ lib/pages/loan.dart
@@ -0,0 +1,11 @@
+class Loan {
+  final String id;
+  final double amount;
+  final double remainingBalance;
+  final DateTime disbursementDate;
+  final DateTime dueDate;
+  final String status;
+  final String type;
+  final List<Payment> payments;
+
+  
