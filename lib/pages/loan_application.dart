38b41efa23090ec3fec97ca6b9093277e88365fc
Lisacode100 <eageno075@gmail.com>
2025-07-01 05:41:02 +0300

    loan application

--- lib/pages/loan_application.dart
+++ lib/pages/loan_application.dart
@@ -13,3 +13,24 @@ class LoanApplicationScreen extends StatefulWidget {
     required this.memberSavings,
     required this.onSubmit,
   });
+
+    @override
+  State<LoanApplicationScreen> createState() => _LoanApplicationScreenState();
+}
+
+class _LoanApplicationScreenState extends State<LoanApplicationScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final _amountController = TextEditingController();
+  final _purposeController = TextEditingController();
+  
+  String _loanType = 'Personal';
+  int _repaymentPeriod = 6;
+  List<PlatformFile> _documents = [];
+  bool _isSubmitting = false;
+
+  @override
+  void dispose() {
+    _amountController.dispose();
+    _purposeController.dispose();
+    super.dispose();
+  }
