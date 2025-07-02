17ec83777c0f14126c4f315a1318cfb306649c36
Zoe691 <zoetino07@gmail.com>
2025-07-02 07:52:00 +0300

    Added information for voice navigation to work

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,9 +1,16 @@
-allprojects {
+buildscript {
     repositories {
         google()
         mavenCentral()
     }
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.4.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.0")
+    }
 }
+
+
+
 
 val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
 rootProject.layout.buildDirectory.value(newBuildDir)
