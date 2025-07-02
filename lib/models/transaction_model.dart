3ca8e4bf0b942e8119164785c934a164492e72bd
Lisacode100 <eageno075@gmail.com>
2025-07-02 09:20:56 +0300

    added payment transaction file

--- lib/models/transaction_model.dart
+++ lib/models/transaction_model.dart
@@ -25,4 +25,45 @@ class Transaction {
   String getFormattedDate() => DateFormat('MMM d, y').format(date);
   String getAmountText() => NumberFormat.currency(symbol: 'UGX ').format(amount);
 
+  Color getStatusColor() {
+    switch (status) {
+      case 'Completed':
+        return Colors.green;
+      case 'Pending':
+        return Colors.orange;
+      case 'Failed':
+        return Colors.red;
+      default:
+        return Colors.grey;
+    }
+  }
+
+   factory Transaction.fromJson(Map<String, dynamic> json) {
+    return Transaction(
+      id: json['id'],
+      amount: json['amount'].toDouble(),
+      date: DateTime.parse(json['date']),
+      type: json['type'],
+      status: json['status'],
+      method: json['method'],
+      reference: json['reference'],
+      phoneNumber: json['phoneNumber'],
+    );
+  }
+
+  Map<String, dynamic> toJson() => {
+    'id': id,
+    'amount': amount,
+    'date': date.toIso8601String(),
+    'type': type,
+    'status': status,
+    'method': method,
+    'reference': reference,
+    'phoneNumber': phoneNumber,
+  };
+}
+
+
+
+
 
