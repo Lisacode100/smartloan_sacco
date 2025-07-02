c850364c1a4e9ea937209c34094214c955c8e6e5
Lisacode100 <eageno075@gmail.com>
2025-07-02 09:33:19 +0300

    app notification

--- lib/models/notification_model.dart
+++ lib/models/notification_model.dart
@@ -13,3 +13,27 @@ class AppNotification {
   bool isRead;
   final String? actionUrl;
 
+  AppNotification({
+    required this.id,
+    required this.title,
+    required this.message,
+    required this.date,
+    required this.type,
+    this.isRead = false,
+    this.actionUrl,
+  });
+
+  factory AppNotification.fromJson(Map<String, dynamic> json) {
+    return AppNotification(
+      id: json['id'],
+      title: json['title'],
+      message: json['message'],
+      date: DateTime.parse(json['date']),
+      type: NotificationType.values[json['type']],
+      isRead: json['isRead'],
+      actionUrl: json['actionUrl'],
+    );
+  }
+
+
+
