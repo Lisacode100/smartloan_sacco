66e140e03257156d4385a3fa7341e2af34886417
Lisacode100 <eageno075@gmail.com>
2025-07-02 09:38:06 +0300

    momo deposit

--- lib/models/deposit_model.dart
+++ lib/models/deposit_model.dart
@@ -6,3 +6,25 @@ class Deposit {
   final String status;
   final String reference;
 
+    Deposit({
+    required this.id,
+    required this.amount,
+    required this.date,
+    required this.method,
+    required this.status,
+    required this.reference,
+  });
+
+  factory Deposit.fromJson(Map<String, dynamic> json) {
+    return Deposit(
+      id: json['id'],
+      amount: json['amount'].toDouble(),
+      date: DateTime.parse(json['date']),
+      method: json['method'],
+      status: json['status'],
+      reference: json['reference'],
+    );
+  }
+}
+
+
